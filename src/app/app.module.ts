9981a8f882adfd40a4cfdad759b64e2575bd8185
Alex Mellnik <a.r.mellnik@gmail.com>
2018-03-15 20:03:30 -0700

    Add flux as module
    Also add a default component, "home"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { ClarityModule } from "@clr/angular";
+import { BSON } from "bson";
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '**', component: HomeComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } 
+    ),
     BrowserModule,
-    ClarityModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
